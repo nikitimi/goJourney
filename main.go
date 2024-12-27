35d6ba40cf08a59f229bc6528fd8ab8665a9bac8
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2024-12-27 14:51:50 +0800

    chore: corrected the module name of the repository.

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
-
-	"dev/go/practice/hello"
 )
 
+func doPanic() {
+	defer func() { // Anonymous function.
+		if e := recover(); e != nil {
+			fmt.Printf("Recovering with %s\n", e)
+		}
+	}()
+	panic("I'm panicking AHHH!")
+	fmt.Println("This will be unreachable code.")
+}
+
 func main() {
-	fmt.Println(hello.String())
+	fmt.Println("Initializing panicking hormone.")
+	doPanic()
+	fmt.Println("Deym! recovered.")
 }
