c8e46cdb3d1af61bf7c84871f3c370335bdfef6c
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2024-12-29 00:13:36 +0800

    feat: passing function as handler.

--- main.go
+++ main.go
@@ -5,22 +5,15 @@ import (
 	"net/http"
 )
 
-type messageHandler struct {
-	message string
-}
-
-func (m *messageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, m.message)
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to Go Web Development")
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mh1 := &messageHandler{"Welcome to Go Web Development"}
+	mh1 := http.HandlerFunc(messageHandler)
 	mux.Handle("/welcome", mh1)
 
-	mh2 := &messageHandler{"This is awesome!"}
-	mux.Handle("/message", mh2)
-	// fs := http.FileServer(http.Dir("public"))
 	http.ListenAndServe(":8080", mux)
 }
