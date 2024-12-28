6a36d97143a055e2a5b0b83c0b535d85edc0af77
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2024-12-29 00:09:10 +0800

    feat: verbose way of creating custom handler.

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+type messageHandler struct {
+	message string
+}
+
+func (m *messageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, m.message)
+}
+
 func main() {
 	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir("public"))
-	mux.Handle("/", fs)
+
+	mh1 := &messageHandler{"Welcome to Go Web Development"}
+	mux.Handle("/welcome", mh1)
+
+	mh2 := &messageHandler{"This is awesome!"}
+	mux.Handle("/message", mh2)
+	// fs := http.FileServer(http.Dir("public"))
 	http.ListenAndServe(":8080", mux)
 }
