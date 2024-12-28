e513cae316cdd1c89c4077af4283e95cd62b4c6e
Gian Carlo Carranza <giancarloacarranza@gmail.com>
2024-12-28 20:53:08 +0800

    feat: added enum with goroutines.

--- hello/hello.go
+++
@@ -1,5 +0,0 @@
-package hello
-
-func String() string {
-	return "Hello World!"
-}
--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+	"unicode"
+)
 
-	"dev/go/practice/hello"
+var wg sync.WaitGroup
+
+func labeler(str string) {
+	defer wg.Done()
+	limit := 2
+	for count := 0; count < limit; count++ {
+		sleep := rand.Int63n(1000)
+		time.Sleep(time.Duration(sleep) * time.Millisecond)
+		fmt.Printf("Count %d from %s\n", count+1, str)
+	}
+}
+
+const (
+	ALPHABET string = "abcdefghijklmnopqrstuvwxyz"
 )
 
 func main() {
-	fmt.Println(hello.String())
+	start := time.Now()
+	wg.Add(len(ALPHABET))
+	fmt.Println("Waiting to finish.")
+	for _, r := range ALPHABET {
+		buf := &bytes.Buffer{}
+		buf.WriteRune(unicode.ToUpper(r))
+		go labeler(buf.String())
+	}
+	wg.Wait()
+	end := time.Now()
+	fmt.Printf("Terminating program.\nElapsed period: %v", end.Sub(start))
 }
